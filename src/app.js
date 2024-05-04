d04bfcfe9fb0f608bffba010bff2d389d5214f85
thomasisa1 <thomasisa1@users.noreply.github.com>
2024-05-04 03:53:49 +0000

    card Generator - bootstrap

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+document.getElementById("drawButton").addEventListener("click", function() {
+  const suits = ["♥", "♦", "♣", "♠"];
+  const ranks = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  const card = document.querySelector(".card");
+  const rankElement = document.querySelector(".rank");
+  const suitElements = document.querySelectorAll(".suit");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  const suit = suits[Math.floor(Math.random() * suits.length)];
+  const cardRank = ranks[Math.floor(Math.random() * ranks.length)];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  rankElement.textContent = cardRank;
+  suitElements.forEach(element => {
+    element.textContent = suit;
+    element.style.color = suit === "♥" || suit === "♦" ? "red" : "black";
+  });
+});
